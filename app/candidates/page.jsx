fd9c3508dadaf107b479f055bbffad8d3d789bb2
rsastri21 <rsastri21@gmail.com>
2023-01-18 00:39:58 -0800

    Minor updates to widths.

--- app/candidates/page.jsx
+++ app/candidates/page.jsx
@@ -14,7 +14,7 @@ export default function Candidate() {
     
     return (
         <div className="bg-gradient-to-r from-cyan-200 to-teal-200 flex flex-col min-h-screen h-auto w-screen pb-16">
-            <div className="w-1/2 h-min min-w-half mx-auto justify-center">
+            <div className="w-2/3 h-min min-w-half mx-auto justify-center">
                 <h1 className="pt-24 pb-12 px-8 text-8xl text-center text-gray-800">
                    Candidate Home. 
                 </h1>
@@ -23,7 +23,7 @@ export default function Candidate() {
                 </p>
                 <hr className="h-px mt-8 mx-auto bg-gray-800 border-0 w-2/3 items-center"></hr>
             </div>
-            <div className="w-1/2 max-w-3xl min-w-min h-min py-4 my-8 mx-auto flex flex-col space-y-12">
+            <div className="w-[50%] min-w-min h-min py-4 my-8 mx-auto flex flex-col space-y-12 items-center">
                 <CandidateTable />
                 <HeadersUI toggle={toggleForm} />
                 <HeaderForm showHeaderForm={showHeaderForm} toggle={toggleForm} />
