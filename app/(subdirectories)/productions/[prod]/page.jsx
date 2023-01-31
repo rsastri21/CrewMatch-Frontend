054e4acbb597ed3b0cbda66541ec703b929c9ac1
rsastri21 <rsastri21@gmail.com>
2023-01-30 21:21:24 -0800

    Implemented delete functionality for a production.

--- app/(subdirectories)/productions/[prod]/page.jsx
+++ app/(subdirectories)/productions/[prod]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect, useLayoutEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import CandidateTable from '../../candidates/CandidateTable';
 import { FcFilmReel } from "react-icons/fc";
 import { useSession } from '../../../SessionContext';
@@ -17,6 +18,7 @@ export default function ProductionUI({ params, }) {
     
     const [production, setProduction] = useState({});
     const [tableVisible, setTableVisible] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
@@ -33,6 +35,10 @@ export default function ProductionUI({ params, }) {
     const toggle = (index) => {
         setIndex(index);
         setTableVisible(!tableVisible);
+    }
+
+    const toggleDelete = () => {
+        setDeleteModal(!deleteModal);
     }
     
     return (
@@ -60,6 +66,8 @@ export default function ProductionUI({ params, }) {
                 </h1>
                 {production && <CandidateTable fetchURL={API_URL + `/api/candidate/search?assigned=false&actingInterest=true&production=${production.name}`} mode="actor" />}
             </section>
+            <DeleteModal id={production.id} visible={deleteModal} setVisible={toggleDelete} />
+            <DeleteProductionBox visible={deleteModal} setVisible={toggleDelete} />
         </div>
     );
 }
@@ -113,4 +121,87 @@ function AvailableCandidateModal({ production, visible, toggleModal, role, index
             </section>
         </div>
     );
+}
+
+function DeleteProductionBox({ visible, setVisible }) {
+    return (
+        <section className="w-1/4 min-w-min h-auto shadow-md mx-auto my-16 bg-white rounded-2xl flex flex-col">
+            <div className="w-full min-w-min h-16 bg-red-300 rounded-t-2xl shadow-md">
+                <h1 className="px-3 py-4 text-gray-900 font-medium text-2xl min-w-fit">Delete Production</h1>
+            </div>
+            <div className="box-border p-4 w-full h-min rounded-b-2xl bg-red-100 flex flex-col items-center space-y-6">
+                <p className="p-2 text-lg text-gray-900 bg-red-50 rounded-lg">
+                    If this production was created accidentally, or it needs to be removed from the list of active productions, it can be deleted here.
+                    <br></br>
+                    <span className="font-semibold">Warning:</span> This action cannot be undone, so please ensure that the deletion of this production is intentional.
+                </p>
+                <button onClick={() => setVisible()} className="p-4 w-fit font-medium text-lg text-gray-100 bg-gradient-to-r from-red-500 to-rose-500 rounded-lg shadow-md 
+                                                hover:shadow-lg hover:bg-gradient-to-r hover:from-red-600 hover:to-rose-600 
+                                                active:bg-gradient-to-r active:from-red-700 active:to-rose-700">
+                    Delete Production
+                </button>
+            </div>
+        </section>
+    )
+}
+
+function DeleteModal({ id, visible, setVisible }) {
+    
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    
+    const handleEscPress = (event) => {
+        if (visible && event.key === 'Escape') {
+            setVisible();
+        }
+    };
+
+    useEffect(() => {
+        // Event listener
+        document.addEventListener('keydown', handleEscPress);
+
+        // Remove event listener
+        return () => {
+            document.removeEventListener('keydown', handleEscPress);
+        };
+    }, [visible]);
+
+    const handleDeletePress = (e) => {
+        
+        setLoading(true);
+        e.preventDefault();
+
+        const requestOptions = {
+            method: 'DELETE'
+        }
+        fetch(API_URL + "/api/production/delete/" + id, requestOptions)
+            .then((res) => res.text())
+            .catch(err => console.error(err))
+            .finally((result) => {
+                setLoading(false);
+                router.push("/productions");
+            });
+    }
+    
+    return (
+        visible && 
+        <div className="fixed bottom-0 left-0 right-0 z-10 w-screen h-screen p-4 bg-gray-700 bg-opacity-50 flex flex-col justify-center items-center">
+            <section className="w-1/4 h-auto bg-white rounded-2xl flex flex-col box-border p-4 shadow-2xl">
+                <h1 className="px-3 py-4 font-medium text-2xl text-center">Are you sure you want to delete this production?</h1>
+                <p className="text-lg text-center font-normal px-3 py-2 ">This action cannot be undone.</p>
+                <div className="w-full h-auto flex space-x-4 justify-center box-border p-4">
+                    <button onClick={(e) => handleDeletePress(e)} className="p-4 w-fit font-medium text-lg text-gray-100 bg-gradient-to-r from-red-500 to-rose-500 rounded-lg shadow-md 
+                                                    hover:shadow-lg hover:bg-gradient-to-r hover:from-red-600 hover:to-rose-600 
+                                                    active:bg-gradient-to-r active:from-red-700 active:to-rose-700">
+                        Confirm Deletion
+                    </button>
+                    <button onClick={() => setVisible()} className="p-4 w-fit font-medium text-lg text-gray-100 bg-gradient-to-r from-green-500 to-emerald-500 rounded-lg shadow-md 
+                                                    hover:shadow-lg hover:bg-gradient-to-r hover:from-green-600 hover:to-emerald-600 
+                                                    active:bg-gradient-to-r active:from-green-700 active:to-emerald-700">
+                        Return to Page
+                    </button>
+                </div>
+            </section>
+        </div>
+    );   
 }
