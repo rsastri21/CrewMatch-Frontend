59e37805a2d27b020ae4259dd89cfdd509f685ad
rsastri21 <rsastri21@gmail.com>
2023-02-07 17:24:35 -0800

    User edit menu in progress.

--- app/(subdirectories)/admin/page.jsx
+++ app/(subdirectories)/admin/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import { Transition } from "@headlessui/react";
 
 export default function AdminUI() {
     return (
@@ -25,7 +27,7 @@ function ManageUsers() {
             <div className="bg-white h-fit w-full rounded-t-2xl drop-shadow-md flex">
                 <h1 className="px-3 py-4 font-medium text-2xl">Manage Users</h1>
             </div>
-            <div className="box-border p-2 w-full min-w-fit h-fit max-h-128 rounded-b-2xl overflow-y-scroll">
+            <div className="box-border p-2 w-full max-h-128 overflow-y-scroll">
                 <UserTable />
             </div>
         </section>
@@ -35,16 +37,36 @@ function ManageUsers() {
 function UserTable() {
     
     const [users, setUsers] = useState([]);
+    const [productions, setProductions] = useState([]);
+    const [userIndex, setUserIndex] = useState(0);
+    const [visible, setVisible] = useState(false);
+
+    const handleUsernameClick = (event, index) => {
+        setUserIndex(index)
+        toggle();
+    }
+
+    const toggle = () => {
+        setVisible(!visible);
+    }
 
     useEffect(() => {
-        const get = async () => {
+        const getUsers = async () => {
             const res = await fetch(process.env.API_URL + '/api/user/get');
             const data = await res.json();
 
             setUsers(data);
         }
 
-        get().catch(console.error);
+        const getProds = async () => {
+            const res = await fetch(process.env.API_URL + '/api/production/get');
+            const data = await res.json();
+
+            setProductions(data);
+        }
+
+        getUsers().catch(console.error);
+        getProds().catch(console.error);
     }, []);
 
     if (users.length === 0) {
@@ -54,32 +76,119 @@ function UserTable() {
     }
 
     return (
-        <table className="table-auto border-separate w-full min-w-fit">
-            <thead>
-                <tr>
-                    <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300 rounded-tl-lg">Username</th>
-                    <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300">Role</th>
-                    <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300 rounded-tr-lg">Production Lead For</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((user, index) => (
-                    <tr key={user.id}>
-                        <td className={`text-center border border-slate-300 transition-all ${index === users.length - 1 ? "rounded-bl-lg" : ""}`}>
-                            <button className="p-2 w-full rounded-md bg-white hover:scale-105 hover:shadow-md active:bg-slate-100 transition-all">
-                                {user.username}
-                            </button>
-                        </td>
-                        <td className="text-center py-2 px-2 border border-slate-300">
-                            <span className={`text-slate-100 px-2 py-1 rounded-lg bg-gradient-to-r
-                                ${user.role === "admin" ? "from-green-500 to-emerald-500" : 
-                                (user.role === "production head" ? "from-violet-500 to-purple-500" : 
-                                "from-red-500 to-rose-500")}`}>{user.role}</span>
-                        </td>
-                        <td className={`text-center py-2 px-2 border border-slate-300 ${index === users.length - 1 ? "rounded-br-lg" : ""}`}>{user.leads}</td>
+        <>
+            {visible && 
+            <>
+                <BackgroundOverlay />
+                <EditUser user={users[userIndex]} productions={productions} visible={visible} toggleVisible={setVisible} />
+            </>
+            }
+            
+            <table className="table-auto border-separate w-full min-w-fit">
+                <thead>
+                    <tr>
+                        <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300 rounded-tl-lg">Username</th>
+                        <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300">Role</th>
+                        <th className="py-2 px-1 font-medium border border-slate-400 bg-slate-300 rounded-tr-lg">Production Lead For</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                        <tr key={user.id}>
+                            <td className={`text-center border border-slate-300 transition-all ${index === users.length - 1 ? "rounded-bl-lg" : ""}`}>
+                                <button
+                                    onClick={(event) => handleUsernameClick(event, index)}
+                                    className="p-2 w-full rounded-md bg-white hover:scale-105 hover:shadow-md active:bg-slate-100 transition-all"
+                                >
+                                    {user.username}
+                                </button>
+                            </td>
+                            <td className="text-center py-2 px-2 border border-slate-300">
+                                <span className={`text-slate-100 px-2 py-1 rounded-lg bg-gradient-to-r
+                                        ${user.role === "admin" ? "from-green-500 to-emerald-500" :
+                                        (user.role === "production head" ? "from-violet-500 to-purple-500" :
+                                            "from-red-500 to-rose-500")}`}>{user.role}</span>
+                            </td>
+                            <td className={`text-center py-2 px-2 border border-slate-300 ${index === users.length - 1 ? "rounded-br-lg" : ""}`}>{user.leads}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
+}
+
+function BackgroundOverlay() {
+    return (
+        <div className="fixed bottom-0 left-0 right-0 z-50 w-screen h-screen p-4 bg-gray-700 bg-opacity-50 flex flex-col justify-center"></div>
+    );
+}
+
+function EditUser({ user, productions, visible, toggleVisible }) {
+    
+    const handleEscPress = (event) => {
+        if (visible && event.key === 'Escape') {
+            toggleVisible();
+        }
+    };
+    
+    useEffect(() => {
+        // Event listener
+        document.addEventListener('keydown', handleEscPress);
+
+        // Remove event listener
+        return () => {
+            document.removeEventListener('keydown', handleEscPress);
+        };
+    }, [visible]);
+
+    return (
+        <div className="fixed bottom-0 left-0 right-0 z-50 w-screen h-screen p-4 flex flex-col justify-center">
+            <section className="mx-auto box-border w-fit min-w-fit h-72 bg-white rounded-2xl shadow-md">
+                <div className="px-4 bg-white z-50 h-fit w-full rounded-t-2xl drop-shadow-md flex justify-center">
+                    <AiOutlineUser className="w-9 h-9 ml-2 mr-1 my-3" />
+                    <h1 className="px-3 py-4 font-medium text-2xl">{user.username}</h1>
+                </div>
+                <section className="box-border p-2 w-full h-fit bg-white rounded-b-2xl flex flex-col items-center">
+                    <div className="text-center py-2 px-2">
+                        <span className={`px-4 py-8 text-slate-100 text-lg shadow-md bg-gradient-to-r ${(user.role === "production head") ? 'from-violet-500 to-purple-500' : ((user.role === "admin") ? 'from-green-500 to-emerald-500' : 'from-red-500 to-rose-500')} px-2 py-1 rounded-lg`}>
+                            {user.role}
+                        </span>
+                    </div>
+                    {!user.leads || user.leads.length > 0 &&
+                        <div>
+                            <span className="px-4 py-8 text-slate-100 text-lg shadow-md bg-gradient-to-r from-emerald-500 to-teal-500 rounded-lg">
+                                {user.leads}
+                            </span>
+                        </div>
+                    }
+                    <form className="w-full h-full box-border p-2 columns-sm">
+                        <div className="mx-auto my-4 w-full shadow-sm border border-slate-400 bg-slate-50 rounded-lg flex justify-between">
+                            <label className="px-3 py-2 font-medium text-lg">Update Role:</label>
+                            <select 
+                                className="px-2 py-2 ml-6 rounded-lg"
+                                defaultValue={user.role}
+                            >
+                                <option value="user">user</option>
+                                <option value="production head">production head</option>
+                                <option value="admin">admin</option>
+                            </select>
+                        </div>
+                        <div className="mx-auto my-4 w-full shadow-sm border border-slate-400 bg-slate-50 rounded-lg flex justify-between">
+                            <label className="px-3 py-2 font-medium text-lg">Assign to Lead Production:</label>
+                            <select
+                                className="px-2 py-2 ml-6 rounded-lg"
+                                defaultValue="empty"
+                            >
+                                <option value="empty"></option>
+                                {productions.map((production) => (
+                                    <option value={production.name}>{production.name}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </form>
+                </section>
+            </section>
+        </div>
+    );
 }
