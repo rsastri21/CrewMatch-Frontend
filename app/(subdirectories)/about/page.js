ba160bc0fc14945660aa243d234624d9eafb3e5f
rsastri21 <rsastri21@gmail.com>
2023-04-03 20:23:44 -0700

    Updated version notes.

--- app/(subdirectories)/about/page.js
+++ app/(subdirectories)/about/page.js
@@ -37,14 +37,21 @@ export default function About() {
                     <h1 className="px-3 py-2 text-3xl font-medium">Changelog</h1>
                     <div className="w-auto mx-2 h-fit shadow-md rounded-xl flex flex-col gap-2 bg-emerald-100">
                         <div className="flex w-full justify-between bg-emerald-200 rounded-t-xl">
-                            <h1 className="px-2 py-2 text-2xl font-medium w-full h-fit">Latest Release: Version 1.1.1</h1>
-                            <span className="px-2 py-1 my-auto mr-3 ml-auto rounded-xl bg-red-500 text-slate-100 text-center text-sm font-medium shadow-md">v1.1.1</span>
+                            <h1 className="px-2 py-2 text-2xl font-medium w-full h-fit">Latest Release: Version 1.1.2</h1>
+                            <span className="px-2 py-1 my-auto mr-3 ml-auto rounded-xl bg-red-500 text-slate-100 text-center text-sm font-medium shadow-md">v1.1.2</span>
                         </div>
-                        <ul className="px-4 py-2 text-xl list-disc list-inside">Production heads/Admins can self-assign as a production lead upon production creation. Fixed an issue with user modification. <br></br>
+                        <ul className="px-4 py-2 text-xl list-disc list-inside">Production heads/Admins can add or remove roles after creating a production. Fixed an issue with whitespace in production naming. <br></br>
                         <span className="font-medium">Current known issues are:</span>
                             <li className="px-3 py-2 text-lg">Transitions being broken on some pop-ups.</li>
                             <li className="px-3 py-2 text-lg">Export CSV function occassionally requiring two attempts to initiate <br></br> &nbsp; &nbsp; &nbsp; non-null file download.</li>
                         </ul>
+                    </div>
+                    <div className="w-auto mx-2 h-fit shadow-md rounded-xl flex flex-col gap-2 bg-slate-100">
+                        <div className="flex w-full justify-between bg-slate-200 rounded-t-xl">
+                            <h1 className="px-2 py-2 text-2xl font-medium w-full h-fit">Version 1.1.1</h1>
+                            <span className="px-2 py-1 my-auto mr-3 ml-auto rounded-xl bg-red-500 text-slate-100 text-center text-sm font-medium shadow-md">v1.1.1</span>
+                        </div>
+                        <ul className="px-4 py-2 text-xl list-disc list-inside">Production heads/Admins can self-assign as a production lead upon production creation. Fixed an issue with user modification.</ul>
                     </div>
                     <div className="w-auto mx-2 h-fit shadow-md rounded-xl flex flex-col gap-2 bg-slate-100">
                         <div className="flex w-full justify-between bg-slate-200 rounded-t-xl">
