f1ce64e579a009b29eb17d86fdfa13284b04f54c
rsastri21 <rsastri21@gmail.com>
2023-04-16 21:55:17 -0700

    Added config options.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
+  const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-  module.exports = {
-    experimental: {
-      appDir: true,
-    },
-    reactStrictMode: true,
-    env: {
-      API_URL: "https://crew-match.herokuapp.com"
+  module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+      return {
+          experimental: {
+            appDir: true,
+          },
+          reactStrictMode: true,
+          env: {
+            API_URL: "http://localhost:8080"
+          }
+      }
     }
+
+    return {
+      experimental: {
+        appDir: true,
+      },
+      reactStrictMode: true,
+      env: {
+        API_URL: "https://crew-match.herokuapp.com"
+      }
+    }
+    
   };
